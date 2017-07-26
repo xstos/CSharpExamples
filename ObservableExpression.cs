47c70a5cebd0f21fcbbc04b1689aa01c783c3745
Chris C <D44471C1FDEC4EB68129F7199C28BFAD@5720EE628AA542F391DA2FA621549BB1.com>
2017-07-26 15:33:32 -0400

    Add ObservableExpression (incomplete, but working)

---
+++ ObservableExpression.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+namespace CSharpExamples
+{
+    public class ObservableExpression<T> : IObservable<T>
+    {
+        Func<T> getValue;
+        Subject<T> subject = new Subject<T>();
+
+        public Func<T> Getter => getValue;
+
+        public ObservableExpression<T> Set(T value)
+        {
+            this.getValue = () => value;
+            subject.OnNext(value);
+            return this;
+        }
+
+        public static implicit operator ObservableExpression<T>(T value)
+        {
+            return new ObservableExpression<T>() { getValue = ()=>value };
+        }
+
+        public static implicit operator Func<T>(ObservableExpression<T> oe)
+        {
+            return oe;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return subject.Subscribe(observer);
+        }
+
+        static Func<T> plus(dynamic first,ObservableExpression<T> second)
+        {
+            return () => first + second.getValue();
+        }
+
+        public static ObservableExpression<T> operator +(ObservableExpression<T> oe1, ObservableExpression<T> oe2)
+        {
+            var ret = new ObservableExpression<T>();
+            oe1.Distinct().Subscribe((i) =>
+            {
+                ret.getValue = plus(i, oe2);
+                ret.OnNext();
+            });
+            oe2.Distinct().Subscribe((i) =>
+            {
+                ret.getValue = plus(i, oe1);
+                ret.OnNext();
+            });
+
+            ret.getValue = () => (dynamic)oe1.getValue() + (dynamic)oe2.getValue();
+            ret.OnNext();
+            return ret;
+        }
+
+        private void OnNext()
+        {
+            this.subject.OnNext(this.getValue());
+        }
+    }
+
+    public static class Example
+    {
+        public static void Main()
+        {
+            ObservableExpression<int> subExpr = 0;
+
+            var myExpr = subExpr + 1000 + subExpr;
+
+            myExpr.Distinct().Subscribe((i) =>
+            {
+                Console.WriteLine(i);
+            });
+
+            subExpr.Set(1);   //prints 1002
+            subExpr.Set(123); //prints 1246
+            
+            Console.ReadLine();
+        }
+    }
+}
